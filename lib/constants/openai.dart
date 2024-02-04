7e07543aa3702dd04468fa600e6cd9a56932d760
Sahil Bansal <bansalsahil315@gmail.com>
2024-02-04 17:39:20 +0530

    Delete lib/constants/openai.dart

--- lib/constants/openai.dart
+++
@@ -1,8 +0,0 @@
-// ignore_for_file: non_constant_identifier_names
-
-
-// First API KEY
-String OPEN_AI_API_KEY = "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";
-// Second API KEY
-String API_KEY = "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";
-String BASE_URL = "https://api.openai.com/v1";
